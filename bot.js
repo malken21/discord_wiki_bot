15852c48afcddd5343dab8b6d84e43ff6d5dd058
malken21 <77374813+malken21@users.noreply.github.com>
2021-10-03 11:05:14 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,21 +1,49 @@
 
-const { Client, Intents} = require("discord.js");
+const { Client, Intents, MessageEmbed, MessageActionRow, MessageButton} = require("discord.js");
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
 const Config = require("./Config.json")
 const wikijs = require("wikijs").default;
 client.login(Config.TOKEN);
 client.on('ready', () => {
-  const time = new Date()
-  console.log(`${time}\nlogin!!(${client.user.tag})`);
+  console.log(`login!!(${client.user.tag})`);
 });
 client.on('messageCreate', message => {
 	if (message.content.startsWith(`${Config.Command} `) && !message.author.bot) {
-		message.channel.sendTyping()
-		let wiki = wikijs({ apiUrl: Config.Wiki_URL });
-		wiki.search(message.content.split(' ')[1]).then(data => {
-			wiki.page(data.results[0])
-			.then(page => page.summary())
-			.then(page => {message.reply({ content: page, allowedMentions: { repliedUser: false }})});
+		const wiki = wikijs({ apiUrl: Config.Wiki_URL });
+		wiki.search(message.content.split(`${Config.Command} `)[1]).then(data => {
+			wiki.page(data.results[0]).then(page => page.summary()).then(page => {
+				wiki.page(data.results[0]).then(page => page.url()).then(page => {
+					wiki.page(data.results[0]).then(page => page.pageImage()).then(page => {
+						wiki.page(data.results[0]).then(page => page.content()).then(page => {
+							const content = page;
+							console.log(content);
+							if(content[0]){
+								const Wiki_MessageEmbed = new MessageEmbed()
+								.setColor('686974')
+								.setTitle(data.results[0])
+								.setThumbnail(pageImage)
+								.setDescription(`${summary}`)
+								.addField(content[0].title,content[0].content)
+								message.reply({ embeds: [Wiki_MessageEmbed], components: [new MessageActionRow().addComponents(new MessageButton().setURL(url).setLabel(`wikipedia URL`).setStyle(`LINK`))], allowedMentions: { repliedUser: false }});
+							}else{
+								const Wiki_MessageEmbed = new MessageEmbed()
+								.setColor('686974')
+								.setTitle(data.results[0])
+								.setThumbnail(pageImage)
+								.setDescription(`${summary}`)
+								message.reply({ embeds: [Wiki_MessageEmbed], components: [new MessageActionRow().addComponents(new MessageButton().setURL(url).setLabel(`wikipedia URL`).setStyle(`LINK`))], allowedMentions: { repliedUser: false }});
+							}
+						});
+						const pageImage = page;
+						console.log(pageImage);
+					});
+					const url = decodeURI(page);
+					console.log(url);
+				});
+				const summary = page;
+				console.log(summary);
+			});
 		});
+		message.channel.sendTyping();
 	}
 });
