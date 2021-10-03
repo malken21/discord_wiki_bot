d42016d75e8138c85499d7a5ec7f1ca1ea6b1a8e
malken21 <77374813+malken21@users.noreply.github.com>
2021-10-03 13:15:10 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,3 @@
-
 const { Client, Intents, MessageEmbed, MessageActionRow, MessageButton} = require("discord.js");
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
 const Config = require("./Config.json")
@@ -16,23 +15,21 @@ client.on('messageCreate', message => {
 					wiki.page(data.results[0]).then(page => page.pageImage()).then(page => {
 						wiki.page(data.results[0]).then(page => page.content()).then(page => {
 							const content = page;
-							console.log(content);
+							let test = "";
 							if(content[0]){
-								const Wiki_MessageEmbed = new MessageEmbed()
-								.setColor('686974')
-								.setTitle(data.results[0])
-								.setThumbnail(pageImage)
-								.setDescription(`${summary}`)
-								.addField(content[0].title,content[0].content)
-								message.reply({ embeds: [Wiki_MessageEmbed], components: [new MessageActionRow().addComponents(new MessageButton().setURL(url).setLabel(`wikipedia URL`).setStyle(`LINK`))], allowedMentions: { repliedUser: false }});
-							}else{
-								const Wiki_MessageEmbed = new MessageEmbed()
-								.setColor('686974')
-								.setTitle(data.results[0])
-								.setThumbnail(pageImage)
-								.setDescription(`${summary}`)
-								message.reply({ embeds: [Wiki_MessageEmbed], components: [new MessageActionRow().addComponents(new MessageButton().setURL(url).setLabel(`wikipedia URL`).setStyle(`LINK`))], allowedMentions: { repliedUser: false }});
+								test = `\n===${content[0].title}===\n${content[0].content}`
+								for(let loop = 0;content[loop].items;loop++){
+									console.log(loop);
+									test = `\n===${content[loop+1].title}===\n${content[loop+1].content}`
+								}
 							}
+							console.log(test);
+							const Wiki_MessageEmbed = new MessageEmbed()
+							.setColor('686974')
+							.setTitle(data.results[0])
+							.setThumbnail(pageImage)
+							.setDescription(`${summary}\n${test}`)
+							message.reply({ embeds: [Wiki_MessageEmbed], components: [new MessageActionRow().addComponents(new MessageButton().setURL(url).setLabel(`wikipedia URL`).setStyle(`LINK`))], allowedMentions: { repliedUser: false }});
 						});
 						const pageImage = page;
 						console.log(pageImage);
